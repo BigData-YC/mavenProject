0848be49195cd6d6e9a40287e123611ce41f6e7a
BigData-YC <1528271058@qq.com>
2019-06-22 16:04:43 +0800

    maven模板提交

--- src/main/java/APP.java
+++ src/main/java/APP.java
@@ -9,6 +9,6 @@
 public class APP {
     public static void main(String[] args) {
         System.out.println("app");
-        System.out.println("hello world!");
+        System.out.println("hello world! ");
     }
 }
