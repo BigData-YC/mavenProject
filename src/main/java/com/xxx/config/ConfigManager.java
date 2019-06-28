788b8c4c8ce2841fc013cfeae16365f905c90849
BigData-YC <1528271058@qq.com>
2019-06-28 14:29:17 +0800

    maven模板提交

---
+++ src/main/java/com/xxx/config/ConfigManager.java
@@ -0,0 +1,14 @@
+package com.xxx.config;
+
+/**
+ * @ClassName: ConfigManager
+ * @Description: TODO
+ * @Author: Yangchong
+ * @Data: 2019/6/25 12:55
+ * @Version: 1.0
+ **/
+
+public class ConfigManager {
+     public static String runENV;
+
+}
