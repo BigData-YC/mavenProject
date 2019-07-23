b2ea7cedb2f3f301c0e70ecda1e1835899f3c427
BigData-YC <1528271058@qq.com>
2019-07-23 14:30:08 +0800

    杨冲：com

--- src/main/java/com/xxx/config/ConfigManager.java
+++ src/main/java/com/xxx/config/ConfigManager.java
@@ -1,5 +1,10 @@
 package com.xxx.config;
 
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.util.Properties;
+
 /**
  * @ClassName: ConfigManager
  * @Description: TODO
@@ -9,6 +14,83 @@ package com.xxx.config;
  **/
 
 public class ConfigManager {
-     public static String runENV;
+    static Logger logger = Logger.getLogger(ConfigManager.class);
+    public static String conf;
+    public static String bin;
+    public static String dev;
 
+    public static void init(String[] args) {
+        if (args.length != 0) {
+            for (int i = 0; i < args.length; i++) {
+                if ("-Denv".equalsIgnoreCase(args[i])) {
+                    if (args.length > i + 1) {
+                        dev = args[i + 1].toLowerCase();
+                        System.setProperty("runENV", args[i + 1].toLowerCase());
+                    }
+                }
+                if ("-config".equalsIgnoreCase(args[i])) {
+                    if (args.length > i + 1) {
+                        conf = args[i + 1].toLowerCase();
+                    }
+                }
+                if ("-class".equalsIgnoreCase(args[i])) {
+                    if (args.length > i + 1) {
+                        bin = args[i + 1].toLowerCase();
+                    }
+                }
+            }
+        }
+    }
+
+    public InputStream getFile2IO(String fileName) {
+        InputStream inputstream = null;
+        String filePath = null;
+        try {
+            filePath = conf + File.separator + dev + File.separator + fileName;
+            System.out.println("load file:" + filePath);
+            inputstream = new FileInputStream(filePath);
+        } catch (FileNotFoundException e) {
+            logger.warn(fileName + e);
+        }
+        return inputstream;
+    }
+
+    /**
+     * 根据文件名字获取配置
+     *
+     * @param fileName
+     * @return
+     */
+    public Properties getFile2Properties(String fileName) {
+        Properties pro = new Properties();
+        try {
+            pro.load(getFile2IO(fileName));
+        } catch (IOException e) {
+            logger.warn(fileName + e);
+        }
+        return pro;
+    }
+
+    public Properties getFile2Properties(File fileName) {
+        Properties pro = new Properties();
+        InputStream inputstream = null;
+        try {
+            inputstream = new FileInputStream(fileName);
+            pro.load(inputstream);
+        } catch (IOException e) {
+            logger.warn(fileName.getAbsolutePath() + e);
+        } finally {
+            try {
+                inputstream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return pro;
+    }
+
+    public static void main(String[] args) {
+        ConfigManager.init(args);
+        System.out.println(ConfigManager.bin);
+    }
 }
